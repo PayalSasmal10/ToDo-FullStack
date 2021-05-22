6c30c68ab0bd3e1f7ded53d1cfc635a6be857346
princekr96 <princekr700@gmail.com>
2021-05-22 17:43:34 +0530

    Created project component

--- frontend/src/containers/project-section.js
+++ frontend/src/containers/project-section.js
@@ -5,18 +5,37 @@ import {
   faBell,
   faQuestionCircle,
   faSearch,
+  faHome,
+  faTasks,
+  faCalendar,
+  faCog,
+  faSignOutAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import Header from '../components/header/header';
 import SideNav from '../components/side-nav/side-nav';
+import Projects from '../components/projects/projects';
 import './project-section.scss';
 
-library.add(faChevronDown, faBell, faQuestionCircle, faSearch);
+library.add(
+  faChevronDown,
+  faBell,
+  faQuestionCircle,
+  faSearch,
+  faHome,
+  faTasks,
+  faCalendar,
+  faCog,
+  faSignOutAlt
+);
 
 const ProjectSection = () => {
   return (
     <div className="project-section">
       <SideNav />
-      <Header />
+      <div className="main-section">
+        <Header />
+        <Projects />
+      </div>
     </div>
   );
 };
