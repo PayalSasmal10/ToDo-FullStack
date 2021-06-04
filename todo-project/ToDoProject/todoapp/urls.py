fd239cad8f8cd384bb22b362b792a8000d3b9e1e
Payal Sasmal <sasmalpayal@gmail.com>
2021-06-04 20:19:01 +0530

    remove multiple endpoint to single endpoint

--- todo-project/ToDoProject/todoapp/urls.py
+++ todo-project/ToDoProject/todoapp/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverviews, name="api-overview"),
-    path('task-list/', views.taskList, name='task-list'),
-    path('task-details/<str:pk>/', views.taskDetails, name='task-details'),
-    path('task-creation/', views.taskCreation, name='task-creation'),
-    path('task-list/<str:pk>/', views.taskUpdate, name='task-list'),
+    path('task', views.taskList, name='task'),
+    path('task/<str:pk>', views.taskUpdate, name='task'),
     path('task-delete/<str:pk>/', views.taskDelete, name='task-delete')
 ]
