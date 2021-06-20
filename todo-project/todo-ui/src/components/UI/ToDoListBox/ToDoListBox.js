2c7f997ec8c27b9b5ad4b5b213efd9d2ac4683f5
princekr96 <princekr700@gmail.com>
2021-06-20 14:49:07 +0530

    Icons for edit and delete task is added

--- todo-project/todo-ui/src/components/UI/ToDoListBox/ToDoListBox.js
+++ todo-project/todo-ui/src/components/UI/ToDoListBox/ToDoListBox.js
@@ -1,15 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ToDoListBox.scss';
 
 const ToDoListBox = ({ todoTitle, todoNote, status, id, open, setOpen }) => {
   return (
-    <section className="todo-listbox" key={id} onClick={() => setOpen(!open)}>
+    <section className="todo-listbox" key={id}>
       <main className="task-title">
         <span className="dot"></span>
         <p className="note-title">{todoTitle}</p>
       </main>
       <p className="note-paragraph">{todoNote}</p>
-      <div className="edit-icons"></div>
+      <div className="edit-icons">
+        <FontAwesomeIcon
+          icon="edit"
+          className="i-edit"
+          onClick={() => setOpen(!open)}
+        />
+        <FontAwesomeIcon icon="trash-alt" className="i-trash" />
+      </div>
     </section>
   );
 };
