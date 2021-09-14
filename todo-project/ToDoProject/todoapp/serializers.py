2c4757539adbd74dad7818794c03a019d7b301f9
Payal Sasmal <sasmalpayal@gmail.com>
2021-09-14 22:23:59 +0530

    added login view and changed registeration form

--- todo-project/ToDoProject/todoapp/serializers.py
+++ todo-project/ToDoProject/todoapp/serializers.py
@@ -12,14 +12,22 @@ class TaskSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(validated_data['email'], validated_data['password'])
         return user
+
+class LoginSeralizer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    email = serializers.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
