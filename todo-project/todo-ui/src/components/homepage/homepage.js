a2f6d3ecf506f9a1245bc10b2237359cd2419563
princekr96 <princekr700@gmail.com>
2021-11-07 16:24:39 +0530

    lato font added

--- todo-project/todo-ui/src/components/homepage/homepage.js
+++ todo-project/todo-ui/src/components/homepage/homepage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './homepage.scss';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Task Tracker</h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    <div className="homepage">
+      <div className="heading">
+        <h1>Task Tracker</h1>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      </div>
+      <div className="content">
+        <h4>
+          Now managing tasks is so easy and effective using the{' '}
+          <strong>Task Tracker</strong> App.
+        </h4>
+      </div>
     </div>
   );
 };
