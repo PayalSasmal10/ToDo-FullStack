41787674faaf2b4c3ec9696bacf5a8f0cab5606e
princekr96 <princekr700@gmail.com>
2021-11-07 16:47:01 +0530

    modified login page

--- todo-project/todo-ui/src/pages/login-page.js
+++ todo-project/todo-ui/src/pages/login-page.js
@@ -16,12 +16,11 @@ const LoginPage = () => {
           <SignUp loginSwitch={loginSwitch} setLoginSwitch={setLoginSwitch} />
         )}
       </div>
-      <div className="brand-section">
-        {/* <img src={task} alt="Task Tracker" /> */}
+      {/* <div className="brand-section">
         <h2 className="multicolortext">TASK</h2>
         <h2 className="multicolortext">TRACKER</h2>
         #stars #stars2 #stars3
-      </div>
+      </div> */}
     </div>
   );
 };
