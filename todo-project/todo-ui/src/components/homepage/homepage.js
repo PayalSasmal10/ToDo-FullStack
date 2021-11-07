da54be3cbe009cdf474d208064da0db73ae31569
princekr96 <princekr700@gmail.com>
2021-11-07 16:35:46 +0530

    homepage created

--- todo-project/todo-ui/src/components/homepage/homepage.js
+++ todo-project/todo-ui/src/components/homepage/homepage.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import svg from '../../images/task-svg.svg';
 import './homepage.scss';
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="heading">
-        <h1>Task Tracker</h1>
+        <h1>TASK TRACKER</h1>
         <Link to="/login">
           <button>Login</button>
         </Link>
       </div>
       <div className="content">
-        <h4>
-          Now managing tasks is so easy and effective using the{' '}
-          <strong>Task Tracker</strong> App.
-        </h4>
+        <img src={svg} alt="task logo" />
+        <h2>
+          Now managing tasks is so easy using the <strong>Task Tracker</strong>{' '}
+          App.
+        </h2>
       </div>
     </div>
   );
