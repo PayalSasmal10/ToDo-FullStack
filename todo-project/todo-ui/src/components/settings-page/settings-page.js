ffd603c2369a45c0f85cb4571e64a70380895c92
princekr96 <princekr700@gmail.com>
2021-11-11 11:33:38 +0530

    Settings page created and added

---
+++ todo-project/todo-ui/src/components/settings-page/settings-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './settings-page.scss';
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="password">Change Password</div>
+    </div>
+  );
+};
+
+export default Settings;
--- todo-project/todo-ui/src/store/auth-context.js
+++ todo-project/todo-ui/src/store/auth-context.js
@@ -7,6 +7,8 @@ const AuthContext = React.createContext({
   login: (token) => {},
   logout: () => {},
   firstNameSetter: (firstName) => {},
+  settingOpen: false,
+  settingSetter: (settingOpen) => {},
 });
 
 export const AuthContextProvider = (props) => {
@@ -15,6 +17,8 @@ export const AuthContextProvider = (props) => {
 
   const initialName = localStorage.getItem('name');
   const [firstName, setFirstName] = useState(initialName);
+
+  const [isSettingOpen, setIsSettingOpen] = useState('');
 
   const userIsLoggedIn = !!token;
 
@@ -34,13 +38,19 @@ export const AuthContextProvider = (props) => {
     localStorage.setItem('name', firstName);
   };
 
+  const settingHandler = (settingVal) => {
+    setIsSettingOpen(settingVal);
+  };
+
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     firstName: firstName,
+    settingOpen: isSettingOpen,
     login: loginHandler,
     logout: logoutHandler,
     firstNameSetter: firstNameHandler,
+    settingSetter: settingHandler,
   };
 
   return (
