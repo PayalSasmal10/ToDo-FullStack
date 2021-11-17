a467d1c98deacc480543b6a812f0d7ad36f5566b
payal <sasmalpayal@.com>
2021-11-17 20:10:54 +0530

    change password api created

--- todo-project/ToDoProject/todoapp/urls.py
+++ todo-project/ToDoProject/todoapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PasswordCheckTokenAPI, RequestPasswordResetEmail, SignUpView, SignInView, LogOutView, taskList, taskPrimarykeybased, SetNewPasswordAPIView
+from .views import PasswordCheckTokenAPI, RequestPasswordResetEmail, SignUpView, SignInView, LogOutView, taskList, taskPrimarykeybased, SetNewPasswordAPIView, ChangePasswordAPIView
 
 urlpatterns = [
     path('task', taskList.as_view(), name='task'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout', LogOutView.as_view(), name='logout'),
     path('request-reset-email', RequestPasswordResetEmail.as_view(), name='request-reset-email'),
     path('password-reset/<uidbase64>/<token>', PasswordCheckTokenAPI.as_view(), name='password-reset-confirmation'),
-    path('password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete')
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
+    path('change-password', ChangePasswordAPIView.as_view(), name='change-password'),
 ]
