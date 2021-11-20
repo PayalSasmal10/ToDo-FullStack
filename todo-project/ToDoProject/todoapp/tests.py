6f53d73f968a9a9f5c3349aeb647fda16d691a50
payal <sasmalpayal@.com>
2021-11-20 18:53:14 +0530

    added one test cases

--- todo-project/ToDoProject/todoapp/tests.py
+++ todo-project/ToDoProject/todoapp/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+
+from django.contrib.auth.models import User
+from django.http import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from .serializers import RegisterSerializer
+from .models import User, Task
 
 # Create your tests here.
+
+class RegistrationTestAPI(APITestCase):
+
+    def test_registration(self):
+        data = {"first_name": "testcase", "last_name": "lastname", "email":"testcase@todoapp.com", "password": "testcase"}
+
+        response = self.client.post("/app/signup", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
