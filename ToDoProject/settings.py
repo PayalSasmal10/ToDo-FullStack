6cdabd9b89fadd91c06e5d17f8c3b6edfb178574
payal <sasmalpayal@.com>
2021-11-26 16:07:17 +0530

    updated staticfile

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -172,10 +172,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'build','staticfiles')
 # location where you will store your static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'build/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'build/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
