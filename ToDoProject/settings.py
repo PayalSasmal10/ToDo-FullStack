53ab4d32a2d1c3cc2b4e161cbd9cd1b5aac2d1b2
payal <sasmalpayal@.com>
2021-11-26 19:31:36 +0530

    added task.png

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'build','staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'build','static')
 # location where you will store your static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'build/static')
 ]
