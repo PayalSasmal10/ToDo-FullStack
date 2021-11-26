73e736e4e72847528da68225ba075109cf129523
payal <sasmalpayal@.com>
2021-11-26 11:09:47 +0530

    added csrftoken config at app.js

--- ToDoProject/urls.py
+++ ToDoProject/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('app/', include('todoapp.urls')),
+    path('app/', include('todoapp.urls')),
     re_path('',TemplateView.as_view(template_name='index.html')),
 ]
 
