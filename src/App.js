a9ee330e3368900a5288e31329b07fb2e0dede82
princekr96 <princekr700@gmail.com>
2021-12-09 14:28:00 +0530

    favicon update using useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import LoginPage from './pages/login-page';
 import ProjectSection from './pages/project-section';
 import AuthContext from './store/auth-context';
-import './App.css';
 import HomePage from './components/homepage/homepage';
-
+import icon from './images/favicon.ico';
+import './App.css';
 
 function App() {
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
-  console.log('isLoggedIn', isLoggedIn);
+  // console.log('isLoggedIn', isLoggedIn);
+
+  // Changing favicon
+  useEffect(() => {
+    const favicon = document.getElementById('favicon');
+    favicon.setAttribute('href', icon);
+  });
+
   return (
     <div className="App">
       <Route path="/" exact>
